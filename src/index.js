da6c4063ebcbd983a77e74de7b2b93f2c6928051
ogoodo <ogoodo.com@qq.com>
2016-03-17 11:18:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export default function thunkMiddleware({ dispatch, getState }) {
       return action(dispatch, getState);
     }
 
-    // 这里的next其实是store.dispatch(或者包装过的store.dispatch)
+    // 这里的next其实是store.dispatch(或者包装过的store.dispatch即其它中间件的action=>{}这个函数(看传入的参数都是action就好理解))
     // redux/applyMiddleware.js源码：  dispatch = compose(...chain)(store.dispatch)
     return next(action);
   };
